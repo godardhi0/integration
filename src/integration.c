f21da5b8eb6e97725f6be9fa480d98b8e462f6c1
godardhi0 <ishimwegodardquanta66@gmail.com>
2025-05-09 12:58:56 +0200

    "new commit"

--- src/integration.c
+++ src/integration.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
+#define PI 3.14159265358979323846
+
 
 typedef struct integration
 {
-	double a,b,dx;
+	double width,dx;
 	double area_left;
 	double area_right;
-	
+
 }Rieman_over;
 
 double _sin_function(double x)
@@ -15,38 +17,41 @@ double _sin_function(double x)
 	return sin(x);
 }
 
+double _linear_function(double x)
+{
+	return (x+1);
+}
+
 Rieman_over _constant_dx(double a, double b, double dx)
 {
 
 	Rieman_over func;
 
-	func.a = a;
-	func.b = b;
+	func.width = (b - a) / dx;
 	func.dx = dx;
-	func.area_left = 0.0f;
-	func.area_right = 0.0f;
+	func.area_left = 0.0;
+	func.area_right = 0.0;
 
-	//left riemann sum 
-	for(double i=func.a; i<=func.b ; i+=func.dx)
-	{
-		func.area_left+= _sin_function(i)*func.dx;
-	}
+	 // Left Riemann sum
+	 for (int i = 0; i < func.dx; i++) {
+        double x = a + i * func.width;
+        func.area_left += _linear_function(x) * func.width;
+    }
 
-	//right riemann sum 
-	
-	for(double i=func.b; i>=func.a ; i-=func.dx)
-	{
-		func.area_right+= _sin_function(i)*func.dx;
-	}	
+    // Right Riemann sum
+    for (int i = 1; i <= func.dx; i++) {
+        double x = a + i * func.width;
+        func.area_right += _linear_function(x) * func.width;
+    }
 
 	return func;
 }
 
 int main(void)
 {
-	Rieman_over _sin = _constant_dx(0.0f, 3.0f, 0.1f);
+	Rieman_over _linear = _constant_dx(0.0, 3.0, 1000000.0);
 
-	printf("%f,%f\n", _sin.area_left, _sin.area_right);
+	printf("%f,%f\n", _linear.area_left, _linear.area_right);
 	
 	return 0;
 }
